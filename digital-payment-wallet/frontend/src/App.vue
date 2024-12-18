576d5db57a382db59a5757635db8c502a35ffa8f
Venkateswararaobhimasingi <srinu19773@gmail.com>
2024-12-18 19:40:45 +0530

    code

---
+++ digital-payment-wallet/frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Login from './components/Login.vue';
+</script>
+
+<template>
+  <div id="app">
+    <Login />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  text-align: center;
+}
+</style>
